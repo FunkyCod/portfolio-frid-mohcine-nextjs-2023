a1b5fc0caeb8f035a8993188f35c746bea5b4968
frid <mohcinefrid21@gmail.com>
2023-03-04 19:01:19 +0200

    add site-map

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteUrl:
+    process.env.SITE_URL ||
+    'https://portfolio-frid-mohcine-nextjs-2023.netlify.app',
+  generateRobotsTxt: true, // (optional)
+  sitemapSize: 7000,
+};
